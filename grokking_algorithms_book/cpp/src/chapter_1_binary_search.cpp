7fac953dcdef7a3c31c8423c29e6555e5d2e6a8f
Roman Nikolaenkov <romanliveshere@gmail.com>
2024-06-17 18:58:35 -0700

    grokking algorithms binary search in cpp

--- grokking_algorithms_book/cpp/src/chapter_1_binary_search.cpp
+++ grokking_algorithms_book/cpp/src/chapter_1_binary_search.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 
-void binary_search() { std::cout << "Binary Search"; }
+int binary_search() {
+  int arr[5] = {1, 8, 12, 32, 47};
+  int target = 32;
+  int low = 0;
+  int high = (sizeof(arr) / sizeof(int)) - 1;
+  while (low <= high) {
+    int mid = (high + low) / 2;
+    if (arr[mid] == target) {
+      return mid;
+    }
+    if (arr[mid] > target) {
+      high = mid - 1;
+    }
+    if (arr[mid] < target) {
+      low = mid + 1;
+    }
+  }
 
-void chapter_1() { binary_search(); }
+  return -1;
+}
+
+void chapter_1() {
+  int idx = binary_search();
+  std::cout << "Target found at index: " << idx << "\n";
+}
