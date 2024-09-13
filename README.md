85144815e9926e602c20f6a1a562fc97e4bdf093
Roman Nikolaenkov <romanliveshere@gmail.com>
2024-09-13 00:41:54 -0700

    add grokking algorithms book selection sort

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ cargon run
 ```bash
 # to compile and run
 cd cpp
-g++ src/main.cpp src/chapter_1_binary_search.cpp -o target/binary_search && target/binary_search
+g++ -std=c++17 src/main.cpp src/chapter_1_binary_search.cpp src/chapter_2_selection_sort.cpp -o target/binary_search && target/binary_search
 ```
--- grokking_algorithms_book/cpp/src/chapter_2_selection_sort.cpp
+++ grokking_algorithms_book/cpp/src/chapter_2_selection_sort.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
+#include <vector>
 
-void selection_sort() { std::cout << "Selection Sort\n"; }
+int findSmallest(std::vector<int> arr) {
+  int smallestIndex = 0;
+  int smallest = arr[0];
+
+  for (int i = 1; i < arr.size(); i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+
+  return smallestIndex;
+}
+
+void printArray(std::vector<int> numbers) {
+  for (int number : numbers) {
+    std::cout << number << " ";
+  }
+  std::cout << std::endl;
+}
+
+void selection_sort() {
+  std::vector<int> sortedArray;
+  std::vector<int> unsortedArray = {8, 11, 4, 9, 2, 3};
+
+  std::cout << "Selection Sort\n";
+  std::cout << "Unsorted array ";
+  printArray(unsortedArray);
+  int size = unsortedArray.size();
+
+  for (int i = 0; i < size; i++) {
+    int smallestIndex = findSmallest(unsortedArray);
+    sortedArray.push_back(unsortedArray[smallestIndex]);
+    unsortedArray.erase(unsortedArray.begin() + smallestIndex);
+  }
+  std::cout << "Sorted array ";
+
+  printArray(sortedArray);
+}
 
 void chapter_2() { selection_sort(); }
